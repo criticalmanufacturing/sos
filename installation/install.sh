8ca6a55994feddd079be7008f343b813c5d04ffc
Skidz0r <61596013+Skidz0r@users.noreply.github.com>
2026-03-26 21:24:26 +0000

    chore(sos): adding installation manual (#10)
    Co-authored-by: Rui Jorge <RuiJorge@criticalmanufacturing.com>

--- installation/install.sh
+++ installation/install.sh
@@ -1,11 +1,11 @@
 # This script install cmf-sos and its dependencies on a Linux system. It should be run with root privileges.
-# This can be obtained using "curl -O https://github.com/criticalmanufacturing/sos/raw/refs/heads/main/installation/install.sh" and then "sudo bash install.sh".
+# This can be obtained using "curl -OL https://github.com/criticalmanufacturing/sos/raw/refs/heads/main/installation/install.sh" and then "sudo bash install.sh".
 
 echo "Removing any existing cmf-sos installation..."
 rm -f /usr/local/bin/cmf-sos
 
 echo "Installing cmf-sos and its dependencies..."
-curl -O https://github.com/criticalmanufacturing/sos/releases/download/1.0.0/cmf-sos
+curl -OL https://github.com/criticalmanufacturing/sos/releases/download/v1.0.0/cmf-sos
 mv cmf-sos /usr/local/bin/
 chmod +x /usr/local/bin/cmf-sos
 
